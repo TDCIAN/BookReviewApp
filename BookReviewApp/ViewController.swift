dd3d4dffde1935a0b8290c0faa7446a530ff8b9b
TDCIAN <tdcian71@gmail.com>
2022-03-15 10:10:14 +0900

    add dependencies and config base

---
+++ BookReviewApp/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  BookReviewApp
+//
+//  Created by JeongminKim on 2022/03/15.
+//
+
+import UIKit
+
+final class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .cyan
+    }
+}
